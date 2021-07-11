8339a07d689f2b5f6e4b19d333eb03cf4b4f657b
Youngsu Lim <lys7120@gmail.com>
2021-07-11 20:55:12 +0900

    [MOD] github pages 배포를 위해 docusaurus.config 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,13 +5,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://godzil8.til.dev',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'godzil8', // Usually your GitHub org/user name.
+  projectName: 'til', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
