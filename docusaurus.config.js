c69a9ca4e0bdc598d3dfff8495fb546507f51b6b
Youngsu Lim <lys7120@gmail.com>
2021-07-17 13:56:04 +0900

    [MOD] url github page 규칙에 맞게 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://til.godzil8.gdev',
+  url: 'https://godzil8.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'error',
